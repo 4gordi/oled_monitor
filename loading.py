7b51023e86f5f878cf9bc181a45aaffb19a86e41
4gordi <4gordi@gmail.com>
2023-08-10 10:40:19 +0300

    Update loading.py

--- loading.py
+++ loading.py
@@ -19,15 +19,15 @@ draw.rectangle((0,0,width,height), outline=0, fill=0)
 font = ImageFont.truetype('editundo.ttf', 30)
 
 start_time = time.time()
-timeout = 9
+timeout = 5
 
 while time.time() < start_time + timeout:
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    for i in range(5):
+    for i in range(4):
     string_to_print = 'loading'+'.'*i
 
-    draw.text((8, 15), string_to_print, font=font, fill='white')
+    draw.text((8, 20), string_to_print, font=font, fill='white')
     oled.image(image)
     oled.show()
     time.sleep(0.2)
