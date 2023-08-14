24e43bbaeaa47947d3548156503756796ac6a9d5
4gordi <4gordi@gmail.com>
2023-08-14 08:57:08 +0300

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#The new cron job
+CRON_ADD='@reboot cd /home/$(whoami)/oled_monitor && python3 loading.py && python3 monitor.py &'
+
+#Get crontab and remove lines containing "something" with sed
+#CRON=$(crontab -u $(whoami) -l | sed -n "/something/!p")
+
+#Make sure crontab ends with newline
+if [ $(printf "$CRON" | tail -c 1 | wc -l) -eq 0 ]; then
+  CRON="$CRON\n"
+fi
+
+#Modify the crontab
+printf "$CRON$CRON_ADD\n" | crontab -u $(whoami) -
+
