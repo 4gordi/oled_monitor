b3d98a89b65cda37b7731858f6cbaa3f3686a49c
4gordi <4gordi@gmail.com>
2023-08-14 12:49:31 +0300

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -4,7 +4,7 @@
 CRON_ADD='@reboot cd /home/pi/oled_monitor && python3 loading.py && python3 monitor.py &'
 
 #Get crontab and remove lines containing "something" with sed
-#CRON=$(crontab -u $(whoami) -l | sed -n "/something/!p")
+#CRON=$(crontab -u pi -l | sed -n "/something/!p")
 
 #Make sure crontab ends with newline
 if [ $(printf "$CRON" | tail -c 1 | wc -l) -eq 0 ]; then
