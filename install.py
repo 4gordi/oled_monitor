f38dc7fd108a01e1aace9d40dc61422d12b7dd24
4gordi <4gordi@gmail.com>
2023-08-14 10:34:29 +0300

    Update install.py

--- install.py
+++ install.py
@@ -30,10 +30,14 @@ shell.run_command("sudo cp shutdown.service /etc/systemd/system/shutdown.service
 
 shell.run_command("systemctl enable shutdown.service")
 
+shell.run_command("cp crontab.sh /home/pi/crontab.sh")
+
 shell.run_command("cd ~")
 
 shell.run_command("chmod +x crontab.sh")
 
 shell.run_command("./crontab.sh")
+
+shell.run_command("sudo rm crontab.sh")
 
 shell.prompt_reboot()
